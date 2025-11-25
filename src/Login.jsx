f19dcdab9b9da24ca705879fc377b11253a3d8b9
React116 <mertgul@paxgroupglobal.com>
2025-11-26 00:08:40 +0300

    Linkler guncellendi

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('https://pax-backend-9m4q.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
